548624177910ebb68899dbf07e2a819308e780ae
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2019-07-23 14:31:41 +0800

    CRUD function to LocalStorage

--- src/app/service/local-storage.service.ts
+++ src/app/service/local-storage.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalStorageService {
 
-  constructor() { }
+  constructor(
+    public storage:Storage
+  ) { }
+
+  create_data(key,data){
+    this.storage.set(key,data);
+  }
+
+  read_data(key){
+    this.storage.get(key)
+      return new Promise((resolve,reject)=>{
+        this.storage.get(key).then(resp=>{
+          resolve(resp);
+        },err=>{
+          reject(err)
+        })
+      })
+  }
+
+  delete_data(key){
+    this.storage.remove(key);
+  }
+
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from '../service/local-storage.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  key:any;
+  data:any;
+  retrieve_key:any;
+  retrieve_data:any;
 
+  constructor(
+    public lss:LocalStorageService
+  ) {}
+
+  set_data(){
+    this.lss.create_data(this.key,this.data);
+  }
+
+  get_data(){
+    this.lss.read_data(this.retrieve_key).then(response=>{
+      this.retrieve_data=response;
+    },err=>{
+      console.log("ERROR",err);
+    })
+  }
+
+  delete_data(){
+    this.lss.delete_data(this.retrieve_key);
+  }
 }
